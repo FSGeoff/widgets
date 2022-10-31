59b8b9cbd492e0062572c0ace647ff6f483d86fd
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-31 07:14:44 -0400

    built accordion menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import React from 'react';
+import Accordion from "./components/Accordion";
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'A front-end Javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'Because its dope!'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'Let\'s find out now'
+    }
+];
 
 
-function App() {
-  return (
-    <div>
-     Geoff Nix
-    </div>
-  );
+export const App = () => {
+    return (
+        <div>
+            <Accordion items={items}/>
+        </div>
+    );
 }
 
 export default App;
