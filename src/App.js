53f972ebdca2bfa964831cef11ffaa08a820f80d
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-11-01 09:30:31 -0400

    dropdown completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
@@ -6,11 +6,20 @@ import {options} from './arrays/Options';
 import {items} from "./arrays/items";
 
 export const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
+
     return (
         <div>
+            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
             {/*<Accordion items={items}/>*/}
             {/*<Search />*/}
-            <Dropdown options={options}/>
+            { showDropdown ?
+            <Dropdown options={options}
+                      selected={selected}
+                      onSelectedChange={setSelected}/>
+                : null
+            }
         </div>
     );
 }
